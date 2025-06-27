e45d84cdd40ac7bd2b494c0205f862244fae75a9
Gajendra Honnaiah <gajendraph@gmail.com>
2025-06-26 20:33:52 -0500

    Initial commit - front-end

---
+++ front-end/src/pages/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return (
+        <>
+        <h1>Page Not Found</h1>
+        <p>The link you accessed must be broken or doesnot exist..</p>
+        </>
+    );
+}
